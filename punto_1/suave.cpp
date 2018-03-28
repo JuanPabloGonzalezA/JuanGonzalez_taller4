3903b3296237a7947fa6585b02776c4667117fd5
Juan Gonzalez <jp.gonzalez15@uniandes.edu.co>
2018-03-28 15:08:22 -0500

    imagen punto 1

---
+++ punto_1/suave.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <png.h>
+using namespace std;
+
+int main(){
+	int ancho, alto;
+	png_byte color;
+	png_byte profundidad;
+	png_bytep *punteros_filas;
+	return 0;
+}
+void leer_png(char *archivo){
+	FILE *fp = fopen(archivo, "rb");
+	
+}
